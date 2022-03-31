848ddf5b114029a571980f250248be0a8df158e9
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2022-03-30 21:19:01 -0300

    FS - Feat: Mais alteraçoes do fs

--- bin/Plugins/FS/FS.js
+++ bin/Plugins/FS/FS.js
@@ -1,29 +1,25 @@
 export default class FS {
-
     services = {
         "--help": this.help,
         "-h": this.help,
-    }
-    
+    };
 
-    controller = async (action, args)=>{
-        if(this.services[action]){
+    controller = async (action, args) => {
+        if (this.services[action]) {
             return await this.services[action](args);
         } else {
             error(`Ação ${action} não encontrada na classe FS`);
         }
-    }
+    };
 
+    touch;
 
-
-
-
-    async help(){
+    async help() {
         help("myta git", [
             {
                 name: "pull",
                 description: "Realiza git pull --rebase",
             },
-        ])
+        ]);
     }
 }
