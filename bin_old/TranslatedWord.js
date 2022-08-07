95ac61b04408954ebac58622c8b7006c874019e5
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2022-08-07 14:47:21 -0300

    Iniciando migração para ts

---
+++ bin_old/TranslatedWord.js
@@ -0,0 +1,8 @@
+const TranslatedWords = {
+    "name": "nome",
+    "Name": "Nome",
+    "DefaultBranchNameTemplate": "Template padrão para nome de branch",
+    "DefaultCommitTemplate": "Template padrão para commits",
+}
+
+export default TranslatedWords;
