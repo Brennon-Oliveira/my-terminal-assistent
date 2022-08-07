3224f0f03e023256a1f9dc70863e8a4d0679a133
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2022-08-07 16:04:01 -0300

    Removendo js do projeto

--- bin_old/Consts.js
+++
@@ -1,4 +0,0 @@
-
-export const SETTINGS_FOLDER = `${process.env.HOME || process.env.USERPROFILE}/.myta`;
-export const SETTINGS = `${process.env.HOME || process.env.USERPROFILE}/.myta/settings.json`;
-export const LOCAL_STORAGE = `${process.env.HOME || process.env.USERPROFILE}/.myta/localStorage.json`;
--- bin_old/TranslatedWord.js
+++
@@ -1,8 +0,0 @@
-const TranslatedWords = {
-    "name": "nome",
-    "Name": "Nome",
-    "DefaultBranchNameTemplate": "Template padrão para nome de branch",
-    "DefaultCommitTemplate": "Template padrão para commits",
-}
-
-export default TranslatedWords;
