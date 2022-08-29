bddae70351bf552473ce636908d8fc204b150116
Brennon Gabriel de Oliveira <brennon.oliveira@docato.com.br>
2022-08-29 03:19:57 -0300

    Correção no config

--- bin/Config.ts
+++ bin/Config.ts
@@ -144,7 +144,7 @@ class Config {
         let plugin = await this.utils.exec(`sudo ls "${SETTINGS_FOLDER}/Plugins"`);
         if(plugin.stderr.includes("No such file or directory")){
             await this.utils.exec(`sudo mkdir -p ${SETTINGS_FOLDER}/Plugins`);
-            await this.utils.exec(`sudo cp -r /usr/share/myta/app/bin/Plugins/* ${SETTINGS_FOLDER}/Plugins/`);
+            await this.utils.exec(`sudo cp -r /usr/share/myta/app/bin/Plugins/ ${SETTINGS_FOLDER}`);
         }
 
         await fsPromises.writeFile(SETTINGS, JSON.stringify(settings))
